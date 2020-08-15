d8c7d608483b445e58e0776d2ee125af49e95a41
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-15 19:38:59 +0700

    Initial App Structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
 
-function App() {
-  return <h1>Hello World</h1>;
-}
-
-render(<App />, document.getElementById("root"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
