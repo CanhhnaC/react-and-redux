2ae37a577a35e12676c9411e3ac6ad3c490d7908
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-16 09:52:12 +0700

    Redux Flow

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,61 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+
+import * as courseActions from "../../redux/actions/courseActions";
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.actions.createCourse(this.state.course);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Course</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange.bind(this)}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="save" />
+        {this.props.courses.map((course) => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </form>
+    );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { Provider as ReduxProvider } from "react-redux";
 import App from "./components/App";
+import configureStore from "./redux/configureStore";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("root")
 );
