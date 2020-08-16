01f9c18811a4e1beea455182023fa9e0480c6aed
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-16 20:04:24 +0700

    Async Writes in Redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
-import { Redirect } from "react-router-dom";
-
-import * as courseActions from "../../redux/actions/courseActions";
-import * as authorAction from "../../redux/actions/authorActions";
-
 import CourseList from "./CourseList";
+import { Redirect } from "react-router-dom";
 
 class CoursesPage extends React.Component {
   state = {
-    redirectToAddCoursePage: false,
+    redirectToAddCoursePage: false
   };
 
   componentDidMount() {
     const { courses, authors, actions } = this.props;
+
     if (courses.length === 0) {
-      actions.loadCourses().catch((error) => {
-        alert("loading courses failed " + error);
+      actions.loadCourses().catch(error => {
+        alert("Loading courses failed" + error);
       });
     }
+
     if (authors.length === 0) {
-      actions.loadAuthors().catch((error) => {
-        alert("loading authors failed " + error);
+      actions.loadAuthors().catch(error => {
+        alert("Loading authors failed" + error);
       });
     }
   }
@@ -32,8 +32,8 @@ class CoursesPage extends React.Component {
     return (
       <>
         {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
+        <h2>Courses</h2>
 
-        <h2>Course</h2>
         <button
           style={{ marginBottom: 20 }}
           className="btn btn-primary add-course"
@@ -41,6 +41,7 @@ class CoursesPage extends React.Component {
         >
           Add Course
         </button>
+
         <CourseList courses={this.props.courses} />
       </>
     );
@@ -50,7 +51,7 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
@@ -58,14 +59,13 @@ function mapStateToProps(state) {
     courses:
       state.authors.length === 0
         ? []
-        : state.courses.map((course) => {
+        : state.courses.map(course => {
             return {
               ...course,
-              authorName: state.authors.find((a) => a.id === course.authorId)
-                .name,
+              authorName: state.authors.find(a => a.id === course.authorId).name
             };
           }),
-    authors: state.authors,
+    authors: state.authors
   };
 }
 
@@ -73,9 +73,12 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorAction.loadAuthors, dispatch),
-    },
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
